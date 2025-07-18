f1c891a44cfb14bb7ae585f10515af898bd68e4b
Utshav Dhakal <udhakal970@gmail.com>
2025-07-18 12:20:28 +0545

    Handle whitespace and comment

--- Tokenizer.py
+++ Tokenizer.py
@@ -3,26 +3,36 @@ from Tokens import *
 
 class Tokenizer:
     def __init__(self, source):
+        
+        self.curr = 0
+
+        self.tokens = []
         self.source = source
 
-        self.start = 0
-        self.curr = 0
-        
-        self.tokens = []
+    def forward(self):
+        self.curr += 1
+    
+    def peek(self, offset=1):
+        return self.source[self.curr + offset]
         
     def tokenize(self):
         while (self.curr < len(self.source)):
-            char = self.source[self.curr]
+            char = self.peek(offset=0)
 
-            if char ==  '+':
+            if char == ' ':
+                pass
+            elif char ==  '+':
                 self.tokens.append(Token(TOKEN_PLUS, char))
             elif char == '-':
-                self.tokens.append(Token(TOKEN_MINUS, char))
+                if self.peek() == '-':
+                    while (self.peek(offset=0) != '\n'): self.forward()
+                else:
+                    self.tokens.append(Token(TOKEN_MINUS, char))
             elif char == '*':
                 self.tokens.append(Token(TOKEN_MUL, char))
             elif char == '/':
                 self.tokens.append(Token(TOKEN_DIV, char))
-
-            self.curr += 1
+            
+            self.forward()
 
         return self.tokens
