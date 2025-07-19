968e7914be1e99e71c04bc17bd831eefeae57a55
Utshav Dhakal <udhakal970@gmail.com>
2025-07-20 01:27:54 +0545

    Add support for INTEGER and FLOAT tokens

--- Tokenizer.py
+++ Tokenizer.py
@@ -3,6 +3,7 @@ from Tokens import *
 
 class Tokenizer:
     def __init__(self, source):
+        self.start = 0
         self.curr = 0
 
         self.tokens = []
@@ -17,8 +18,8 @@ class Tokenizer:
     def current(self):
         return self.source[self.curr]
     
-    def add_token(self, token, lexeme):
-        self.tokens.append(Token(token, lexeme))
+    def add_token(self, token):
+        self.tokens.append(Token(token, self.source[self.start : self.curr + 1]))
 
     def match(self, char):
         if self.current() == char: return True
@@ -33,58 +34,69 @@ class Tokenizer:
     def tokenize(self):
         
         while (self.curr < len(self.source)):
+            self.start = self.curr
             char = self.current()
 
             if self.match(' '):
                 pass
 
             elif self.match(';'):
-                self.add_token(SEMICOLON, char)
+                self.add_token(SEMICOLON)
 
             elif self.match( '+'):
-                self.add_token(PLUS, char)
+                self.add_token(PLUS)
 
             elif self.match('-'):
                 if self.peek() == '-':
                     while (self.current() != '\n'): self.forward()
                 else:
-                    self.add_token(MINUS, char)
+                    self.add_token(MINUS)
 
             elif self.match('*'):
-                self.add_token(MUL, char)
+                self.add_token(MUL)
 
             elif self.match('/'):
-                self.add_token(DIV, char)
+                self.add_token(DIV)
 
             elif self.match(':'):
                 if self.match_forward('='):
-                    self.add_token(ASSIGNMENT, ':=')
+                    self.add_token(ASSIGNMENT)
                 else:
-                    self.add_token(UNKNOWN, char)
+                    self.add_token(UNKNOWN)
 
             elif self.match('='):
                 if self.match_forward('='):
-                    self.add_token(EQUAL, '==')
+                    self.add_token(EQUAL)
                 else:
-                    self.add_token(UNKNOWN, char)
+                    self.add_token(UNKNOWN)
                     
             elif self.match('~'):
                 if self.match_forward('='):
-                    self.add_token(NOT_EQUAL, '~=')
+                    self.add_token(NOT_EQUAL)
                 else:
-                    self.add_token(NOT, char)
+                    self.add_token(NOT)
 
             elif self.match('<'):
                 if self.match_forward('='):
-                    self.add_token(LESS_OR_EQUAL, '<=')
+                    self.add_token(LESS_OR_EQUAL)
                 else:
-                    self.add_token(LESS_THAN, char)
+                    self.add_token(LESS_THAN)
 
             elif self.match('>'):
                 if self.match_forward('='):
-                    self.add_token(GREATER_OR_EQUAL, '<=')
+                    self.add_token(GREATER_OR_EQUAL)
                 else:
-                    self.add_token(GREATER_THAN, char)
+                    self.add_token(GREATER_THAN)
+
+            elif char.isdigit():
+                while self.peek().isdigit(): self.forward()
+
+                if self.match_forward('.'):
+                    while self.peek().isdigit(): self.forward()
+                    self.add_token(FLOAT)
+                else:
+                    self.add_token(INTEGER)
+            
             
             self.forward()
 
